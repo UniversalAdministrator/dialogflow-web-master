2f9413c6b4f1793ef5b625b556621e5e477f36ea
Hari sebastian <saihari.adabala@hotmail.com>
2018-01-15 18:45:26 +0530

    ha
    yah

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import { Vue2Dragula } from 'vue2-dragula'
+
+import App from './App.vue'
+
+Vue.use(VueCarousel)
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+}).$mount('#app')
+
+
+
+Vue.use(Vue2Dragula, {
+  logging: { 
+    service: true // to only log methods in service (DragulaService)
+	new Vue({
+	  el: '#app',
+    render: h => h(App)
+}).$mount('#app')
+  }
+});
